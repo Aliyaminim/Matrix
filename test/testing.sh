015fed557c8547a4da332582faec1482d70de750
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-02-14 21:12:07 +0300

    minor hange

--- test/testing.sh
+++ test/testing.sh
@@ -10,7 +10,7 @@ for file in e2e/*.in; do
 done
 
 echo "End-to-end tests for Bareiss algorithm:"
-for file in e2e/test{1..4}.in e2e/test7.in e2e/test8.in e2e/test{11..12}.in e2e/test{14..17}.in; do
+for file in e2e/test{1..4}.in e2e/test7.in e2e/test8.in e2e/test{11..12}.in e2e/test{14..16}.in; do
     echo $file
     ../build/mainll < $file
     echo 
