725bb32363f4ee57204294726cf115fcdc390e43
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-02-17 12:09:07 +0300

    added more tests for gauss and bareiss especially

--- test/testing.sh
+++ test/testing.sh
@@ -3,14 +3,14 @@
 echo Testing has started!
 echo
 echo "End-to-end tests for Gauss algorithm:"
-for file in e2e/*.in; do
+for file in e2e/009.in e2e/010.in e2e/test{1..9}.in e2e/test{10..19}.in e2e/test{20..29}.in; do
     echo $file
     ../build/maind < $file
     echo
 done
 
 echo "End-to-end tests for Bareiss algorithm:"
-for file in e2e/test{1..4}.in e2e/test7.in e2e/test8.in e2e/test{11..12}.in e2e/test{14..16}.in; do
+for file in e2e/009.in e2e/010.in e2e/test{1..4}.in e2e/test7.in e2e/test8.in e2e/test{11..29}.in; do
     echo $file
     ../build/mainll < $file
     echo
